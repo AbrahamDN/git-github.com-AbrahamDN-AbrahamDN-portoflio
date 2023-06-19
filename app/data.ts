9bc125926b6d021f30eb4352c290f8a6b0541a21
AbrahamDN <abe.ndinga@gmail.com>
2023-06-19 02:11:26 +0100

    feat: add data file for skills & links

---
+++ app/data.ts
@@ -0,0 +1,24 @@
+export const skills = {
+  "front-end": [
+    "ReactJS",
+    "NextJS",
+    "SCSS",
+    "Redux",
+    "Javascript",
+    "Typescript",
+  ],
+  "back-end": ["Contentful", "MongoDB", "Firebase", "NodeJS"],
+  others: ["Figma", "Jira"],
+};
+
+export const navLinks = [
+  { title: "About", link: "#about" },
+  { title: "Projects", link: "#projects" },
+  { title: "Contact", link: "#contact" },
+];
+
+export const externalLinks = [
+  { title: "CV", link: "/AbrahamDN-CV-updated.pdf" },
+  { title: "Github", link: "https://github.com/AbrahamDN" },
+  { title: "LinkedIn", link: "https://www.linkedin.com/in/abrahamdn/" },
+];
